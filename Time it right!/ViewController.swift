917983bebffb949c208eeb7d363a797968c11bd3
Martin Cimerman <cim.martin@gmail.com>
2016-05-16 15:38:43 +0200

    Initial Commit

---
+++ Time it right!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time it right!
+//
+//  Created by Martin Cimerman on 16/05/16.
+//  Copyright © 2016 Martin Cimerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
